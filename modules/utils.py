43b3d8cf9df062fe9c879921965c49d451490039
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-04-12 15:50:41 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -89,7 +89,7 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "▰" * completed_length + "▱" * remaining_length
+            progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
                 await reply.edit(f'\n `╭──⌯════🆄︎ᴘʟᴏᴀᴅɪɴɢ⬆️⬆️═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├🤖𝔹ʏ » ˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠\n╰─═══ ✪ @Oye_oo ✪ ═══─╯\n`') 
