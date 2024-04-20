a25391051f3c98aa7412496769766b30c4a750ef
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-04-20 13:52:36 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27498866"
 api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
-bot_token = "6708606494:AAEx0Td-wLOl7RerF_vO678TKnfPrwybecU"
+bot_token = "6909488016:AAHXzuI1N8u9r7cp8Y742PUoezSguvf74fU"
